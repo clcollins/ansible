f9d3516a56a1f7e52884884425a79a034b38f9b7
Christopher Collins <collins.christopher@gmail.com>
2017-04-26 10:44:13 -0400

    adding notes, short url to curl, and removing sudo, since it should explicitely prompt

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 # This script can be downloaded and run to bootstrap the Ansible config
+#
+# https://raw.githubusercontent.com/clcollins/ansible/master/bootstrap.sh
+#
+# Or, a short URL for the above: https://git.io/v9O46
+#
+# Eg: curl -sSL https://git.io/v9O46 -o /tmp/bootstrap.sh
+
+# Currently only Fedora support
 
 ANSIBLE_REPO="https://github.com/clcollins/ansible.git"
 CHECKOUT_DIR="/tmp/ansible-bootstrap"
 
 # Bootstrap for Ansible 
-dnf install -y ansible && sudo ansible-pull -d ${CHECKOUT_DIR} -U ${ANSIBLE_REPO}
+dnf install -y ansible && ansible-pull -d ${CHECKOUT_DIR} -U ${ANSIBLE_REPO}
